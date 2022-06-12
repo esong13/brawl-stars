faaf52d07ac1132aaa14691b6de79efab21a68fd
Midvoy <1416335629@qq.com>
2022-06-12 21:53:52 +0800

    加入Nita，子弹方向调整

--- Classes/Nita.cpp
+++ Classes/Nita.cpp
@@ -1,1 +1,49 @@
-#pragma once
+#include "Nita.h"
+
+bool Nita::init()
+{
+	if (!HeroSprite::init()) return false;
+	setAttackNum(SWORDMAN_ATTACKNUM);//设置攻击力
+	setHealthPointMax(SWORDMAN_HP);//设置血量
+	setHealthPointNow(SWORDMAN_HP);
+	setMoveSpeed(SWORDMAN_SPEED);//设置移动速度
+	setAttackCDTime(SWORDMAN_ATTACKCDTIME);//设置攻速
+
+	return true;
+}
+
+void Nita::heroSetAction(int direction, int num)
+{
+	setAction(direction, "Nita", num);
+}
+
+void Nita::attack(Point targetPosition)
+{
+	if (getBulletNow() <= 0 || attackIsColding) {
+		return;
+	}
+	//攻击冷却
+	setAttackIsColding(true);
+
+	//调用父类函数
+	HeroSprite::attack(targetPosition);
+
+	//Nita攻击：放火
+	Bullet* bulletNode = Bullet::create();
+	bulletNode->bindSprite(Sprite::create("fire.png"));
+
+	bulletNode->getBulletSprite()->setScale(2.0f);
+	bulletNode->getBulletSprite()->setRotation(90.0f);
+	this->addChild(bulletNode);
+
+	//赋予子弹相关值
+	bulletNode->setPosition(getRoleSprite()->getPosition());
+	bulletNode->setBulletDistance(40);
+	bulletNode->setBulletAttackNum(this->getAttackNum());
+	bulletNode->setBulletSpeed(10 * SWORDMAN_SPEED);
+
+	//子弹移动
+	bulletNode->bulletMoveTo(targetPosition);
+
+	bulletHasBeenShot.pushBack(bulletNode);
+}
--- Classes/Nita.h
+++ Classes/Nita.h
@@ -1,1 +1,21 @@
-#pragma once
+#ifndef _NITA_H_
+#define _NITA_H_
+
+#include "cocos2d.h"
+#include "HeroSprite.h"
+USING_NS_CC;
+using namespace cocostudio::timeline;
+
+class Nita : public HeroSprite {
+
+
+public:
+	//创建和初始化
+	CREATE_FUNC(Nita);
+	virtual bool init();
+	//移动
+	void heroSetAction(int direction, int num);
+	//攻击
+	void attack(Point targetPosition);
+};
+#endif
